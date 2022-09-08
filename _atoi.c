841c5ba181db324a10cf1677f132b2d9f6be1d33
Fashemore Toyin <emeraldtoyin@gmail.com>
2022-09-08 08:22:08 +0100

    Added to atoi.c file

--- _atoi.c
+++ _atoi.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
 /**
- * interactive - returns true if shell is interactive mode
- * @info: struct address
+ * interactive - This returns true if shell is interactive mode
+ * @info: Struct address
  *
  * Return: 1 if interactive mode, 0 otherwise
  */
@@ -12,9 +12,9 @@ int interactive(info_t *info)
 }
 
 /**
- * is_delim - checks if character is a delimeter
+ * is_delim - This checks if character is a delimeter
  * @c: the char to check
- * @delim: the delimeter string
+ * @delim: The delimeter string
  * Return: 1 if true, 0 if false
  */
 int is_delim(char c, char *delim)
@@ -26,7 +26,7 @@ int is_delim(char c, char *delim)
 }
 
 /**
- *_isalpha - checks for alphabetic character
+ *_isalpha - This checks for alphabetic character
  *@c: The character to input
  *Return: 1 if c is alphabetic, 0 otherwise
  */
